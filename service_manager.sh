115e41d143ce6c7d5e80728100f2a8727c560acc
Ronei Davi Lach <98993158+rdlach@users.noreply.github.com>
2025-05-09 19:06:25 -0300

    Add files via upload

--- service_manager.sh
+++ service_manager.sh
@@ -5,52 +5,22 @@ USER="opc"
 
 # Comandos de parada por IP
 declare -A hosts_stop=(
-  ["172.24.3.45"]="systemctl stop nginx php-fpm hubba-node-api crond"
-  ["172.24.3.210"]="systemctl stop hubbago.service nginx crond"
-  ["172.24.2.4"]="systemctl stop nginx php-fpm"
-  ["172.24.2.176"]="docker stop \$(docker ps -aq)"
-  ["172.24.2.12"]="docker stop \$(docker ps -aq)"
-  ["172.24.2.83"]="docker stop \$(docker ps -aq)"
-  ["172.24.3.253"]="systemctl stop nginx php-fpm"
-  ["172.24.2.227"]="systemctl stop nginx"
-  ["172.24.3.133"]="systemctl stop gwt-frete-core"
-  ["172.24.3.77"]="systemctl stop gwt-frete.service"
-  ["172.24.3.66"]="systemctl stop gwt-frete.service"
-  ["172.24.3.54"]="systemctl stop gwt-frete.service"
-  ["172.24.3.32"]="systemctl stop gwt-frete-log.service"
-  ["172.24.3.157"]="docker stop \$(docker ps -aq)"
+  ["10.0.0.4"]="systemctl stop nginx"
 )
 
 # Comandos de subida por IP
 declare -A hosts_start=(
-  ["172.24.3.45"]="systemctl start nginx php-fpm hubba-node-api crond"
-  ["172.24.3.210"]="systemctl start hubbago.service nginx crond"
-  ["172.24.2.4"]="systemctl start nginx php-fpm"
-  ["172.24.2.176"]="docker start \$(docker ps -aq)"
-  ["172.24.2.12"]="docker start \$(docker ps -aq)"
-  ["172.24.2.83"]="docker start \$(docker ps -aq)"
-  ["172.24.3.253"]="systemctl start nginx php-fpm"
-  ["172.24.2.227"]="systemctl start nginx"
-  ["172.24.3.133"]="systemctl start gwt-frete-core"
-  ["172.24.3.77"]="systemctl start gwt-frete.service"
-  ["172.24.3.66"]="systemctl start gwt-frete.service"
-  ["172.24.3.54"]="systemctl start gwt-frete.service"
-  ["172.24.3.32"]="systemctl start gwt-frete-log.service"
-  ["172.24.3.157"]="docker start \$(docker ps -aq)"
+  ["172.24.3.45"]="systemctl start nginx"
 )
 
 # Ordem de parada
 order_stop=(
-  172.24.3.45 172.24.3.210 172.24.2.4 172.24.2.176 172.24.2.12
-  172.24.2.83 172.24.3.253 172.24.2.227 172.24.3.133 172.24.3.77
-  172.24.3.66 172.24.3.54 172.24.3.32 172.24.3.157
+  172.24.3.45
 )
 
 # Ordem de subida (reversa)
 order_start=(
-  172.24.3.157 172.24.3.32 172.24.3.54 172.24.3.66 172.24.3.77
-  172.24.3.133 172.24.2.227 172.24.3.253 172.24.2.83 172.24.2.12
-  172.24.2.176 172.24.2.4 172.24.3.210 172.24.3.45
+  172.24.3.45
 )
 
 # Função para parar serviços
